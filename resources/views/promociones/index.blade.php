ef607a6cde4247f5f3e8d229b6c8bbcaf62f9e59
amartinezTG <alejandro.martinez@totalgas.com>
2026-05-14 18:00:46 -0600

    fix: HTTPS en DataTables i18n y proteger .env en gitignore

--- resources/views/promociones/index.blade.php
+++ resources/views/promociones/index.blade.php
@@ -291,7 +291,7 @@
                 },
             ],
             language: {
-                url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
+                url: 'https://cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
             },
             pageLength: 25,
             order: [[6, 'asc']],
